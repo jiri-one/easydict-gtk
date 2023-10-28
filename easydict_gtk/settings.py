d55555ab9b3f1895e62e76575393a5293ab10ee8
jiri-one <nemec@jiri.one>
2023-10-28 10:18:56 +0200

    basic implementation of Settings class

--- easydict_gtk/settings.py
+++ easydict_gtk/settings.py
@@ -1,9 +1,16 @@
 from pathlib import Path
 from os import environ
+import configparser
 
 # internal imports
 from utils import get_xdg_config_home
 
+DEFAULT_SETTINGS = {
+    "win_size_remember": (True, bool),
+    "window_size": ("1200x600", str),
+    "clipboard_scan": (True, bool),
+    "search_language": ("eng", str),
+}
 
 # set current working directory
 cwd = Path(__file__).parent
@@ -14,59 +21,43 @@ for img_path in (cwd / "images").iterdir():
 
 
 cfg_dir = get_xdg_config_home()  # set user config directory
+ini_file = cfg_dir / "easydict.ini"  # set user config file
 
 
 class Settings:
-    def initiate_settings(self):
+    def __init__(self, ini_file: Path):
+        config = configparser.ConfigParser()
+        # check if ini_file exists and if not, create it
+        if not ini_file.exists():
+            config["EASYDICT"] = {key: value[0] for key, value in DEFAULT_SETTINGS}
+            with open(ini_file, "w") as configfile:
+                config.write(configfile)
+        # read the ini file
+        config.read(ini_file)
         try:
-            # get setting of clippboard scan from db and set it
-            pref_clipboard_scan = prefdb.search(query["settings"] == "clipboard_scan")[
-                0
-            ]["value"]
-            self.checkbutton_scan.props.active = pref_clipboard_scan
-            # get setting of window size remembering from db and set it
-            pref_win_size_remember = prefdb.search(
-                query["settings"] == "win_size_remember"
-            )[0]["value"]
-            self.checkbutton_size.props.active = pref_win_size_remember
-            # get the window size from db and set it
-            window_width, window_height = prefdb.search(
-                query["settings"] == "window_size"
-            )[0]["value"]
-            self.window.set_default_size(window_width, window_height)
-            # get setting of search language from db and set it
-            pref_search_language = prefdb.search(
-                query["settings"] == "search_language"
-            )[0]["value"]
-            self.image_language.props.file = str(
-                self.cwd_images / f"flag_{pref_search_language}.svg"
-            )
-            self.language = pref_search_language
-            self.combobox_language.set_active_id(pref_search_language)
-        except IndexError:
-            self.create_default_settings()
-        self.dialog_about.props.version = f"{easydict_gtk.__version__}"
+            ed_config = config["EASYDICT"]
+        except KeyError:
+            raise KeyError("""In easydict.ini file has to be ["EASYDICT"] section!""")
+        # set all keys and values like this object attributes
+        for key, value in ed_config.items():
+            try:
+                value_type = DEFAULT_SETTINGS[key][1]
+            except KeyError as e:
+                raise (
+                    f"In easydict.ini file in section EASYDICT are allowed only known keys \n\n {e}"
+                )
+            except IndexError:
+                raise f"There is some internal error: \n\n {e}"
+            if (
+                value_type == bool
+            ):  # in the future we can use eval for more types or care them separately (like bool, list, ..) which is safer
+                value = ed_config.getboolean(key)
+            setattr(self, key, value)
+        # assert that all keys were read from file and are set
+        try:
+            for key in DEFAULT_SETTINGS:
+                assert hasattr(self, key)
+        except AssertionError as e:
+            raise ValueError(f"Some keys from file were read broken: \n\n {e}")
 
-    def write_setting(self, name, value):
-        prefdb.update({"value": value}, where("settings") == name)
-
-    def create_default_settings(self):
-        # set default settings
-        prefdb.upsert(
-            {"settings": "search_language", "value": "eng"},
-            query["settings"] == "search_language",
-        )
-        prefdb.upsert(
-            {"settings": "clipboard_scan", "value": True},
-            query["settings"] == "clipboard_scan",
-        )
-        prefdb.upsert(
-            {"settings": "window_size", "value": [360, 640]},
-            query["settings"] == "window_size",
-        )
-        prefdb.upsert(
-            {"settings": "win_size_remember", "value": True},
-            query["settings"] == "win_size_remember",
-        )
-        # after default values are set, call initiate_settings again
-        self.initiate_settings()
+ed_setup = Settings(ini_file)
