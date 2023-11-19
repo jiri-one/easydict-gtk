4623ee316f909247c10cfe8c6ab0f4cb601d6fb7
jiri-one <nemec@jiri.one>
2023-11-19 18:02:08 +0100

    test_settings_with_empty_easydict_section finished

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -40,8 +40,24 @@ def test_settings_with_empty_file(tmp_path):
     with pytest.raises(KeyError, match=r"In easydict.ini file has to be \[EASYDICT\] section!"):
         settings = Settings(ini_file)
 
-def test_settings_with_empty_easydict_section(tmp_path):
-    ...
+def test_settings_with_empty_easydict_section(tmp_path, cfg_parser):
+    ini_file = tmp_path / "easydict.ini"  # set user config file
+    ini_file.write_text("[EASYDICT]")
+    assert ini_file.exists()
+    settings = Settings(ini_file)
+    cfg = cfg_parser(ini_file)["EASYDICT"]
+    for key, value_and_type in DEFAULT_SETTINGS.items():
+        type_of_value = value_and_type["type"]
+        default_value = value_and_type["value"]
+        # get actual value from config file or get default value
+        if type_of_value == bool:
+            # getboolean method is case insensitive and care about more variants
+            value = cfg.getboolean(key)
+        elif type_of_value == int:
+            value = cfg.getint(key)
+        else: # value is string
+            value = cfg.get(key)
+        assert getattr(settings, key) == value == default_value
 
 def test_settings_with_non_empty_text_file():
     ...
