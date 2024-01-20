c3bc0df235a3787c068439a7948a3df82c319967
jiri-one <nemec@jiri.one>
2024-01-20 10:19:17 +0100

    improved sqlite backend search

--- easydict_gtk/widgets/result_list.py
+++ easydict_gtk/widgets/result_list.py
@@ -12,7 +12,6 @@ gi.require_version("Gtk", "4.0")
 from gi.repository import Gtk, Gio, GLib, Gdk, GdkPixbuf, GObject, Adw
 
 # internal imports
-from backends.sqlite_backend import search_async
 from settings import ed_setup, LANGUAGES_DATA as lng_data
 from .dialogs import SettingsDialog
 
